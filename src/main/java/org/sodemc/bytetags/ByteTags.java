fde4b66f2fa41fdf8cf5d17fc5d8e36a0f9a95ff
Alex Infanti <ainfanti2@gmail.com>
2023-12-29 13:48:25 -0500

    Initial commit

---
+++ src/main/java/org/sodemc/bytetags/ByteTags.java
@@ -0,0 +1,17 @@
+package org.sodemc.bytetags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ByteTags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
